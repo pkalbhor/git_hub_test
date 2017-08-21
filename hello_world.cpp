d00bc3923203ecb07f3277f8ee9e31847287f9ea
Pritam Kalbhor <pritam.navanath.kalbhor@cern.ch>
2017-08-21 20:39:54 +0530

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,16 @@
+// Hello world program
+
+// This program is to test github terminologies. 
+//
+
+// Auther : Pritam Kalbhor
+// Email : physics.pritam@gmail.com
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello_World \n";
+	return 0;
+}
+
